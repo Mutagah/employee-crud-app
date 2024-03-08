992c90a4c75693a1d184d13d1266dcac8d5d739c
Mutagah <kihahumutagah@gmail.com>
2024-03-08 10:22:25 +0300

    :necktie:feat:Fetch of employees data.

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { EmployeesService } from '../service/employees.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit {
+  title = 'Tabulated Employees';
 
+  constructor(private employees: EmployeesService) {}
+  
+  ngOnInit(): void {
+    this.employees.getEmployees().subscribe({
+      next: (res: any) => {console.log(res)},
+    });
+  }
+  
 }
