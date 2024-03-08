7a20e7d0e3e2bf8e36c530128a0f1d98162bd567
Mutagah <kihahumutagah@gmail.com>
2024-03-08 10:16:56 +0300

    :sparkles:feat:Route Configuration employees and home component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { EmployeesComponent } from './employees/employees.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {
-    path: 'employees',
-    component: EmployeesComponent,
-  },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  // Non existent routes
   {
     path: '**',
     redirectTo: '',
+    pathMatch:'full'
   },
 ];
 
