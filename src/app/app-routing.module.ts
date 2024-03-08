428de7a9fc6a3ad270814117ea767b8c5a7565ee
Mutagah <kihahumutagah@gmail.com>
2024-03-08 16:40:52 +0300

    :sparkles:Addition of routing for the create employee form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { EmployeesComponent } from './employees/employees.component';
 import { HomeComponent } from './home/home.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'employees', component: EmployeesComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
+  {
+    path:'create-employee', component: CreateEmployeeComponent
+  },
   // Non existent routes
   {
     path: '**',
