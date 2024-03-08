51890bbf36520c0523fdce2841b7e89465ac10da
Mutagah <kihahumutagah@gmail.com>
2024-03-08 10:19:33 +0300

    :sparkles:feat:Introduction of Employees service.

---
+++ src/app/service/employees.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeesService {
+  employeesUrl = 'http://localhost:3000/employees';
+ 
+  constructor(private http: HttpClient) {}
+  
+  getEmployees(): Observable<any>{
+    return this.http.get(this.employeesUrl)
+  }
+}
