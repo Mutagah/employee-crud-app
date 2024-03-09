37f4d723e5b77c6917e30b443db07203d9b71c08
Mutagah <kihahumutagah@gmail.com>
2024-03-09 20:27:14 +0300

    :fire:feat:Removal of create-employee.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { EmployeesComponent } from './employees/employees.component';
 import { HomeComponent } from './home/home.component';
-import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
-  {
-    path:'create-employee', component: CreateEmployeeComponent
-  },
   // Non existent routes
   {
     path: '**',
