f4bc4a7178b86869276eef098a8747466b341828
Mutagah <kihahumutagah@gmail.com>
2024-03-10 20:55:06 +0300

    Addition of View Employee functionality.

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { EmployeesService } from '../service/employees.service';
 
+import { EmployeeModalComponent } from '../employee-modal/employee-modal.component';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
@@ -10,7 +13,7 @@ import { EmployeesService } from '../service/employees.service';
 export class EmployeesComponent implements OnInit {
   title = 'Tabulated Employees';
 
-  constructor(private employees: EmployeesService) {}
+  constructor(private employees: EmployeesService, private modal: NgbModal) {}
   employeesList!: any;
   ngOnInit(): void {
     this.getEmployees();
@@ -19,6 +22,20 @@ export class EmployeesComponent implements OnInit {
     this.employees.getEmployees().subscribe({
       next: (res: any) => {
         return (this.employeesList = res);
+      },
+    });
+  }
+
+  viewEmployee(employeeId: number) {
+    /*
+    1.Open a modal that contains the selected employee details
+    2. You need to make a fetch request to gettting a specific employee record.
+     */
+    this.employees.getSpecificEmployee(employeeId).subscribe({
+      next: (res: any) => {
+        const modalRef = this.modal.open(EmployeeModalComponent);
+        modalRef.componentInstance.employeeData = res;
+        console.log(res)
       },
     });
   }
