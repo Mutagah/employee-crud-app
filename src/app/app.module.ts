dde1c0ec0670db1529b947e04507c9917b62c9e5
Mutagah <kihahumutagah@gmail.com>
2024-03-15 10:51:37 +0300

    :lipstick:feat:Addition of toasts upon successful deletion of a record.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgTemplateOutlet } from '@angular/common';
 
 import { EmployeesService } from './service/employees.service';
+import { ToastService } from './toast.service';
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
@@ -13,6 +16,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormModalComponent } from './form-modal/form-modal.component';
 import { DeleteModalComponent } from './delete-modal/delete-modal.component';
+import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,7 @@ import { DeleteModalComponent } from './delete-modal/delete-modal.component';
     NavbarComponent,
     FormModalComponent,
     DeleteModalComponent,
+    ToastComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,8 +34,10 @@ import { DeleteModalComponent } from './delete-modal/delete-modal.component';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
+    NgbToastModule,
+    NgTemplateOutlet
   ],
-  providers: [EmployeesService],
+  providers: [EmployeesService,ToastService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
